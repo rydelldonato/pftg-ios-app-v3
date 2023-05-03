8cff8b89c69bf7f8b8ad92344d8ecba6cdd1088c
Rydell <donatorydell98@gmail.com>
2023-05-02 23:28:39 -0700

    create set up payment modal ui

--- pftg-ios-app-v3/ui/homePage/setUpPayment/setUpPayment.js
+++ pftg-ios-app-v3/ui/homePage/setUpPayment/setUpPayment.js
@@ -1,11 +1,37 @@
-import { View, Text, Modal } from "react-native";
+import {
+  View,
+  Text,
+  Modal,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  TouchableHighlight,
+  StyleSheet,
+} from "react-native";
 import React, { useState } from "react";
+import styles from "../../loginPopUp/styles";
 
 export default function SetUpPayment(props) {
   const { modalVisible, setModalVisible } = props;
 
+  const [cardNumber, setCardNumber] = useState("");
+  const [expirationDate, setExpirationDate] = useState("");
+  const [cvv, setCvv] = useState("");
+
+  const handleSubmit = () => {
+    // Do something with the username and password
+    setModalVisible(!modalVisible);
+    setCardNumber("");
+    setExpirationDate("");
+    window.alert(`Payment information saved`);
+  };
   return (
-    <View>
+    <View
+      style={[
+        styles.loginModal,
+        { justifyContent: "center", alignItems: "center" },
+      ]}
+    >
       <Modal
         animationType="slide"
         transparent={true}
@@ -14,8 +40,61 @@ export default function SetUpPayment(props) {
           Alert.alert("Modal has been closed.");
           setModalVisible = !modalVisible;
         }}
+        style={{ zIndex: 1 }}
       >
-        <Text>setUpPayment</Text>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <TouchableWithoutFeedback
+              hitSlop={{ top: 170, bottom: 170, left: 170, right: 170 }}
+            >
+              <TouchableHighlight
+                style={{ marginLeft: 10, marginTop: 13 }}
+                onPress={() => setModalVisible(!modalVisible)}
+              >
+                <Text style={[styles.exit, { fontSize: 16 }]}>X</Text>
+              </TouchableHighlight>
+            </TouchableWithoutFeedback>
+            <Text style={styles.title}>Set Up Payment</Text>
+            <View>
+              <Text style={styles.label}>Card Number</Text>
+              <TextInput
+                style={styles.input}
+                value={cardNumber}
+                onChangeText={setCardNumber}
+              />
+              <Text style={styles.label}>Expiration Date</Text>
+              <TextInput
+                style={styles.input}
+                value={expirationDate}
+                onChangeText={setExpirationDate}
+              />
+              <Text style={styles.label}>CVV</Text>
+              <TextInput
+                style={styles.input}
+                value={cvv}
+                onChangeText={setCvv}
+                secureTextEntry={true}
+              />
+              <TouchableHighlight
+                onPress={handleSubmit}
+                style={[styles.button, { backgroundColor: "#FAEDCD" }]}
+              >
+                <Text
+                  style={[
+                    styles.buttontext,
+                    {
+                      fontFamily: "K2D_400Regular",
+                      textAlign: "center",
+                      fontSize: 15,
+                    },
+                  ]}
+                >
+                  Submit
+                </Text>
+              </TouchableHighlight>
+            </View>
+          </View>
+        </View>
       </Modal>
     </View>
   );

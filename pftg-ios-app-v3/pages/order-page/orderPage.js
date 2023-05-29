6b02513b73b6b43299186c37cfaba7125cdd72d1
Rydell <donatorydell98@gmail.com>
2023-05-28 21:15:48 -0700

    created ui for pickup/delivery option

--- pftg-ios-app-v3/pages/order-page/orderPage.js
+++ pftg-ios-app-v3/pages/order-page/orderPage.js
@@ -2,6 +2,7 @@ import { View, Text } from "react-native";
 import React, { useState } from "react";
 import GeneralFooter from "../../ui/generalFooter/generalFooter";
 import Header from "../../ui/orderPage/header/header";
+import PickupDeliverySlider from "../../ui/orderPage/pickupDeliverySlider/pickupDeliverySlider";
 
 export default function OrderPage() {
   const [searchPopUp, setSearchPopUp] = useState(false);
@@ -9,6 +10,7 @@ export default function OrderPage() {
   return (
     <View style={{ backgroundColor: "#82B77D", width: "100%", height: "100%" }}>
       <Header />
+      <PickupDeliverySlider />
       <GeneralFooter />
     </View>
   );
---
+++ pftg-ios-app-v3/ui/orderPage/pickupDeliverySlider/pickupDeliverySlider.js
@@ -0,0 +1,79 @@
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import {
+  useFonts,
+  K2D_400Regular,
+  K2D_600SemiBold,
+} from "@expo-google-fonts/k2d";
+
+export default function pickupDeliverySlider() {
+  const [isDeliverySelected, setIsDeliverySelected] = useState(false);
+
+  let [fontsLoaded] = useFonts({
+    K2D_400Regular,
+    K2D_600SemiBold,
+  });
+  if (!fontsLoaded) {
+    return null;
+  }
+  const handleButtonPress = () => {
+    setIsDeliverySelected(!isDeliverySelected);
+  };
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={[
+          styles.button,
+          isDeliverySelected ? styles.buttonSelected : styles.buttonNotSelected,
+        ]}
+        onPress={handleButtonPress}
+      >
+        <Text style={styles.buttonText}>Pickup</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[
+          styles.button,
+          !isDeliverySelected
+            ? styles.buttonSelected
+            : styles.buttonNotSelected,
+        ]}
+        onPress={handleButtonPress}
+      >
+        <Text style={styles.buttonText}>Delivery</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 23,
+  },
+  button: {
+    flex: 1,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: "gray",
+    backgroundColor: "white",
+    marginHorizontal: 5,
+  },
+  buttonSelected: {
+    backgroundColor: "green",
+  },
+  buttonNotSelected: {
+    backgroundColor: "white",
+  },
+  buttonText: {
+    fontSize: 14,
+    fontWeight: "bold",
+    color: "black",
+    textAlign: "center",
+    fontFamily: "K2D_400Regular",
+  },
+});

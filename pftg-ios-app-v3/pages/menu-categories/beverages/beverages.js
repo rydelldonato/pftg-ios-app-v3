6bf41c1c36869b38ec4b368bf27575261abc3691
Rydell <donatorydell98@gmail.com>
2023-06-09 09:29:21 -0700

    fixed beverage page layout

--- pftg-ios-app-v3/pages/menu-categories/beverages/beverages.js
+++ pftg-ios-app-v3/pages/menu-categories/beverages/beverages.js
@@ -1,24 +1,41 @@
-import { View, Text, FlatList, TouchableOpacity } from 'react-native'
-import React from 'react'
-import CategoryLayout from '../../../ui/menuCategories/categoryLayout'
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  Dimensions,
+  Image
+} from "react-native";
+import React, { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import GeneralFooter from "../../../ui/generalFooter/generalFooter";
+import SearchPopUp from "../../../ui/orderPage/searchPopUp/searchPopUp";
 
+const { width } = Dimensions.get("window");
+const middleX = width / 2;
 
 export default function Beverages() {
+  const category = "Beverages";
+  const [searchModal, setSearchModal] = useState(false);
+  const navigation = useNavigation();
 
- const menuItems = [
+
+  const menuItems = [
     {
       name: "Pepsi",
-      description:
-        "Pepsi",
+      description: "Pepsi",
       price: "$2.50",
     },
     {
       name: "Bottled Water",
-      description:
-        "Bottled Water",
+      description: "Bottled Water",
       price: "$1.99",
     },
   ];
+
+  const goBack = () => {
+    navigation.goBack();
+  };
   const renderItem = ({ item }) => {
     if (!item.hasOwnProperty("name")) {
       // Render an empty item
@@ -43,29 +60,80 @@ export default function Beverages() {
         }}
         onPress={() => console.log("Item pressed:", item.name)}
       >
-        
-        <Text
-          style={[
-            
-            { height: "15%", width: "100%", textAlign: "center" },
-          ]}
-        >
+        <Text style={[{ height: "15%", width: "100%", textAlign: "center" }]}>
           {item.name}
         </Text>
-        <Text
-          style={[
-            
-            { height: "15%", width: "100%", textAlign: "center" },
-          ]}
-        >
+        <Text style={[{ height: "15%", width: "100%", textAlign: "center" }]}>
           {item.price}
         </Text>
       </TouchableOpacity>
     );
   };
   return (
-    <View style={{backgroundColor: '#82B77D', height: '100%'}}>
-      <View style={{ height: "80%" }}>
+    <View style={{ backgroundColor: "#82B77D", height: "100%" }}>
+      <View style={{ position: "absolute", top: 50, left: 14, zIndex: 1 }}>
+        <TouchableOpacity onPress={goBack}>
+          <Image source={require("../../../assets/back.png")} />
+        </TouchableOpacity>
+      </View>
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          marginTop: 64,
+          marginLeft: 22,
+          backgroundColor: "#82B77D",
+        }}
+      >
+        <View>
+          <View
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+              width: "100%",
+              paddingHorizontal: middleX - 50,
+              paddingTop: 15,
+            }}
+          >
+            <Text style={{ fontFamily: "Montserrat_700Bold", fontSize: 11, width: '100%' }}>
+              {category}
+            </Text>
+          </View>
+          <SearchPopUp
+            searchModal={searchModal}
+            setSearchModal={setSearchModal}
+          />
+        </View>
+        <View style={{position: 'absolute', left: '90%'}}>
+          <TouchableOpacity
+            style={{ paddingTop: 10 }}
+            onPress={() => {
+              setSearchModal(!searchModal);
+            }}
+          >
+            <Image
+              style={{ width: 20, height: 20 }}
+              source={require("../../../assets/search.png")}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View
+        style={{
+          height: 13,
+          borderBottomColor: "black",
+          borderBottomWidth: 1,
+          shadowColor: "#000",
+          shadowOffset: {
+            width: 0,
+            height: 4,
+          },
+          shadowOpacity: 0.35,
+          shadowRadius: 3.84,
+          elevation: 5,
+        }}
+      ></View>
+      <View style={{ height: "80%", marginTop: 100 }}>
         <FlatList
           style={{
             flex: 1,
@@ -80,6 +148,7 @@ export default function Beverages() {
           renderItem={renderItem}
         />
       </View>
+      <GeneralFooter />
     </View>
-  )
+  );
 }

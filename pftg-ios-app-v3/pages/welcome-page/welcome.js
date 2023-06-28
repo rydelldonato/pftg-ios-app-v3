ef326a7358e3b105040f09d0dd0fdec855de1729
Rydell <donatorydell98@gmail.com>
2023-06-27 20:07:59 -0700

    moved getQuote into component

--- pftg-ios-app-v3/pages/welcome-page/welcome.js
+++ pftg-ios-app-v3/pages/welcome-page/welcome.js
@@ -1,11 +1,4 @@
-import {
-  View,
-  Text,
-  TouchableHighlight,
-  Image,
-  TextInput,
-  Button,
-} from "react-native";
+import { View, Text, TouchableHighlight, Image } from "react-native";
 import SignUp from "../../ui/welcomePage/signUpPopUp/signUp";
 import LoginPopUp from "../../ui/loginPopUp/loginPopUp";
 import React, { useState } from "react";
@@ -15,20 +8,10 @@ import {
   JuliusSansOne_400Regular,
 } from "@expo-google-fonts/julius-sans-one";
 import { K2D_400Regular, K2D_600SemiBold } from "@expo-google-fonts/k2d";
-import { v4 as uuidv4 } from "uuid";
-import { getRandomValues } from "react-native-get-random-values";
 
 export default function Welcome({ navigation }) {
-  uuidv4({ random: getRandomValues });
   const [signUpModalVisible, setSignUpModalVisible] = useState(false);
   const [loginModalVisible, setLoginModalVisible] = useState(false);
-  const [quoteData, setQuoteData] = useState({
-    external_delivery_id: uuidv4({ random: getRandomValues }),
-    pickup_address: "110 North El Dorado Street, Stockton, CA, 95202",
-    pickup_phone_number: 12096848664,
-    dropoff_address: "",
-    dropoff_phone_number: null,
-  });
 
   let [fontsLoaded] = useFonts({
     JuliusSansOne_400Regular,
@@ -38,22 +21,7 @@ export default function Welcome({ navigation }) {
   if (!fontsLoaded) {
     return null;
   }
-  const handleChangeAddress = (text) => {
-    setQuoteData((prevData) => ({
-      ...prevData,
-      dropoff_address: text,
-    }));
-  };
-  const handleChangePhoneNumber = (text) => {
-    setQuoteData((prevData) => ({
-      ...prevData,
-      dropoff_phone_number: text,
-    }));
-  };
-  const handleSubmit = () => {
-    // Handle form submission
-    console.log("Input value:", quoteData);
-  };
+
   return (
     <View style={styles.welcomePage}>
       <SignUp
@@ -66,20 +34,6 @@ export default function Welcome({ navigation }) {
       />
       <View style={styles.logo}>
         <Image source={require("../../assets/welcome-page-logo.png")} />
-      </View>
-      <View style={{ borderWidth: 1 }}>
-        <TextInput
-          style={{ borderWidth: 1 }}
-          placeholder="enter a dropoff address"
-          onChangeText={handleChangeAddress}
-        ></TextInput>
-        <TextInput
-          style={{ borderWidth: 1 }}
-          placeholder="enter a dropoff phone number"
-          keyboardType="numeric"
-          onChangeText={handleChangePhoneNumber}
-        ></TextInput>
-        <Button title="submit" onPress={handleSubmit} />
       </View>
       <View style={styles.titleContainer}>
         <Text
---
+++ pftg-ios-app-v3/ui/orderPage/getQuoteComponent/getQuoteComponent.js
@@ -0,0 +1,49 @@
+import { View, Text, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { getRandomValues } from "react-native-get-random-values";
+
+export default function getQuote() {
+  uuidv4({ random: getRandomValues });
+  const [quoteData, setQuoteData] = useState({
+    external_delivery_id: uuidv4({ random: getRandomValues }),
+    pickup_address: "110 North El Dorado Street, Stockton, CA, 95202",
+    pickup_phone_number: 12096848664,
+    dropoff_address: "",
+    dropoff_phone_number: null,
+  });
+  const handleChangeAddress = (text) => {
+    setQuoteData((prevData) => ({
+      ...prevData,
+      dropoff_address: text,
+    }));
+  };
+  const handleChangePhoneNumber = (text) => {
+    setQuoteData((prevData) => ({
+      ...prevData,
+      dropoff_phone_number: text,
+    }));
+  };
+  const handleSubmit = () => {
+    // Handle form submission
+    console.log("Input value:", quoteData);
+  };
+  return (
+    <View>
+      <View style={{ borderWidth: 1 }}>
+        <TextInput
+          style={{ borderWidth: 1 }}
+          placeholder="enter a dropoff address"
+          onChangeText={handleChangeAddress}
+        ></TextInput>
+        <TextInput
+          style={{ borderWidth: 1 }}
+          placeholder="enter a dropoff phone number"
+          keyboardType="numeric"
+          onChangeText={handleChangePhoneNumber}
+        ></TextInput>
+        <Button title="submit" onPress={handleSubmit} />
+      </View>
+    </View>
+  );
+}

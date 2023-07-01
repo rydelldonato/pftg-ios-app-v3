4a4f7aad1340e08c235b422fe490fe026043d8ed
Rydell <donatorydell98@gmail.com>
2023-06-30 21:47:47 -0700

    removed hardcoded id from menu items

--- pftg-ios-app-v3/backend/menuItems/menuItems.js
+++ pftg-ios-app-v3/backend/menuItems/menuItems.js
@@ -1,6 +1,5 @@
 export default menuItems = [
   {
-    id: 0,
     name: "COMBO #2",
     description:
       "Sisig over Garlic Rice with 5 pcs. of Lumpia and a bottled water.",
@@ -8,7 +7,6 @@ export default menuItems = [
     price: "$16.99",
   },
   {
-    id: 1,
     name: "Shanghai Lumpia (10pcs)",
     description:
       "Traditionally made of ground pork with minced onions, finely chopped carrots and garlic, cilantro and seasonings rolled up in a paper thin egg roll wrapper.",
@@ -16,7 +14,6 @@ export default menuItems = [
     price: "$7.99",
   },
   {
-    id: 2,
     name: "Crispy Sisig Street Tacos (3pcs)",
     description:
       "Crispy deep fried street taco corn tortillas with sisig pork or chicken.",
@@ -24,7 +21,6 @@ export default menuItems = [
     price: "$11.99",
   },
   {
-    id: 3,
     name: "Pork Belly Filipino Asado Bao (2pcs)",
     description:
       "Steamed sweet dough bun stuffed with Pork Belly Filipino style Asado.",
@@ -32,21 +28,18 @@ export default menuItems = [
     price: "$10.99",
   },
   {
-    id: 4,
     name: "Sisig over Garlic Rice",
     description: "Garlic rice topped with sisig pork or chicken.",
     image: require("../../assets/garlicOverRice.png"),
     price: "$12.99",
   },
   {
-    id: 5,
     name: "Sisig Fries",
     description: "Shoestring fries topped with sisig pork or chicken.",
     image: require("../../assets/sisigFries.png"),
     price: "$10.50",
   },
   {
-    id: 6,
     name: "COMBO #1",
     description:
       "3 pcs. of Crispy Sisig Street Tacos with 5 pcs. of Lumpia and a bottled water.",
@@ -54,7 +47,6 @@ export default menuItems = [
     price: "$15.59",
   },
   {
-    id: 7,
     name: "Deep Fried Banana Fritters",
     description:
       "Banana plantain with brown sugar, toasted sesame seeds wrapped in a paper thin egg roll wrapper deep fried. Includes a house made orange liquer sauce.",
@@ -62,14 +54,12 @@ export default menuItems = [
     price: "$5.99",
   },
   {
-    id: 8,
     name: "Sisig Bao (2pcs)",
     description: "Steamed sweet dough bun stuffed with sisig.",
     image: require("../../assets/sisigBao.png"),
     price: "$10.49",
   },
   {
-    id: 9,
     name: "Tofu Asado Bao",
     description:
       "Steamed sweet dough bun stuffed with Filipino Asado flavored Tofu.",
@@ -77,14 +67,12 @@ export default menuItems = [
     price: "$9.99",
   },
   {
-    id: 10,
     name: "Pepsi",
     description:
       "Pepsi",
     price: "$2.50",
   },
   {
-    id: 11,
     name: "Bottled Water",
     description:
       "Bottled Water",

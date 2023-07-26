e8c227dddf7469bbb6238bb373fe7647e9a273af
Rydell <donatorydell98@gmail.com>
2023-07-25 23:26:17 -0700

    added text elements

--- pftg-ios-app-v3/pages/reviewAndPay/reviewAndPay.js
+++ pftg-ios-app-v3/pages/reviewAndPay/reviewAndPay.js
@@ -1,5 +1,6 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 import React, { useEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
 
 export default function ReviewAndPay({ setCurrentPage }) {
   // Call the setCurrentPage function when the component mounts
@@ -7,9 +8,39 @@ export default function ReviewAndPay({ setCurrentPage }) {
     setCurrentPage("Review and Pay");
   }, []);
 
+  const navigation = useNavigation();
+
+  const goBack = () => {
+    setCurrentPage("");
+    navigation.goBack();
+  };
+
   return (
-    <View>
-      <Text>review-and-pay</Text>
+    <View style={{ flex: 1, backgroundColor: "#82B77D" }}>
+      <View style={{ position: "absolute", top: 50, left: 14, zIndex: 1 }}>
+        <TouchableOpacity onPress={goBack}>
+          <Image source={require("../../assets/back.png")} />
+        </TouchableOpacity>
+      </View>
+      <View style={{ display: "flex", marginVertical: 120, marginLeft: 14 }}>
+        <View style={{marginBottom: 15}}>
+        <Text style={{ fontSize: 25, fontFamily: "Montserrat_700Bold" }}>
+          Pick up at
+        </Text>
+        </View>
+        <View style={{ display: "flex", flexDirection: "row",marginBottom: 10 }}>
+          <Image source={require("../../assets/Location.png")} />
+          <Text style={{ fontSize: 15, fontFamily: "Montserrat_300Light" }}>
+            110 North El Dorado Street, Stockton
+          </Text>
+        </View>
+        <View style={{ display: "flex", flexDirection: "row", marginLeft: 10 }}>
+          <Image source={require("../../assets/important.png")} />
+          <Text style={{ fontSize: 14, fontFamily: "Montserrat_700Bold" }}>
+            Pickup closed at 2:45pm
+          </Text>
+        </View>
+      </View>
     </View>
   );
 }
